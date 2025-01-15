69a99f8b0ed2c7985300f1b3b4a656a40d265dbb
Calvin Fang <cfang@guidewire.com>
2025-01-14 23:50:06 -0800

    add log 9

--- utils/fetch-difference.js
+++ utils/fetch-difference.js
@@ -1,10 +1,10 @@
 import config from "../config/index.js";
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 import * as fs from "fs";
 
-const openai = new OpenAIApi(new Configuration({
+const openai = new OpenAI({
   apiKey: config.OPENAI_API_KEY,
-}));
+});
 
 const fetchDifference = async() => {
   // Step 2: Create an Assistant
