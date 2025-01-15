b57604a991e072ca5f57f9619552418ed773adcb
Calvin Fang <cfang@guidewire.com>
2025-01-14 22:38:15 -0800

    add log 3

---
+++ utils/fetch-difference.cjs
@@ -0,0 +1,118 @@
+const config = require("../config/index.js");
+const OpenAI = require("openai");
+const openai = new OpenAI({
+  apiKey: config.OPENAI_API_KEY,
+});
+const fs = require("fs");
+
+const fetchDifference = async() => {
+  // Step 2: Create an Assistant
+  const myAssistant = await openai.beta.assistants.create({
+    model: "gpt-4o-mini",
+    instructions:
+      "You are a customer support chatbot. Use your knowledge base to best respond to customer queries.",
+    name: "Customer Support Chatbot",
+    tools: [{ type: "file_search" }],
+  });
+  console.log("This is the assistant object: ", myAssistant, "\n");
+
+  // Step 1: Upload a File with an "assistants" purpose
+  const myFile = await openai.files.create({
+    file: fs.createReadStream("request_0000014524.json"),
+    purpose: "assistants",
+  });
+  console.log("This is the file object: ", myFile, "\n");
+
+  const myFile2 = await openai.files.create({
+    file: fs.createReadStream("request_0000014524 copy.json"),
+    purpose: "assistants",
+  });
+  console.log("This is the file object: ", myFile2, "\n");
+
+  // Step 3: Create a Thread
+  const myThread = await openai.beta.threads.create();
+  console.log("This is the thread object: ", myThread, "\n");
+
+  // Step 4: Add a Message to a Thread
+  const myThreadMessage = await openai.beta.threads.messages.create(
+    (thread_id = myThread.id),
+    {
+      role: "user",
+      content: "Compare Driver's Name, Voilation Number, Accident Number, Number of Claims between submission quote and renewal quotes.",
+      attachments: [
+        {"file_id": myFile.id, "tools": [{"type": "file_search"}]},
+       {"file_id": myFile2.id, "tools": [{"type": "file_search"}]}
+      ],
+    }
+  );
+  console.log("This is the message object: ", myThreadMessage, "\n");
+
+//   const myThreadMessage2 = await openai.beta.threads.messages.create(
+//     (thread_id = myThread.id),
+//     {
+//       role: "user",
+//       content: "What is the account number?",
+//       attachments: [
+//  //       {"file_id": myFile.id, "tools": [{"type": "file_search"}]},
+//         {"file_id": myFile2.id, "tools": [{"type": "file_search"}]}
+//       ],
+//     }
+//   );
+//   console.log("This is the message object: ", myThreadMessage2, "\n");
+
+  // Step 5: Run the Assistant
+  const myRun = await openai.beta.threads.runs.create(
+    (thread_id = myThread.id),
+    {
+      assistant_id: myAssistant.id,
+      instructions: "Please address the user as Future Insurance.",
+    }
+  );
+  console.log("This is the run object: ", myRun, "\n");
+
+  // Step 6: Periodically retrieve the Run to check on its status to see if it has moved to completed
+  const retrieveRun = async () => {
+    let keepRetrievingRun;
+    let result;
+
+    while (myRun.status === "queued" || myRun.status === "in_progress") {
+      keepRetrievingRun = await openai.beta.threads.runs.retrieve(
+        (thread_id = myThread.id),
+        (run_id = myRun.id)
+      );
+      console.log(`Run status: ${keepRetrievingRun.status}`);
+
+      if (keepRetrievingRun.status === "completed") {
+        console.log("\n");
+
+        // Step 7: Retrieve the Messages added by the Assistant to the Thread
+        const allMessages = await openai.beta.threads.messages.list(
+          (thread_id = myThread.id)
+        );
+
+        console.log(
+          "------------------------------------------------------------ \n"
+        );
+
+        console.log("User: ", myThreadMessage.content[0].text.value);
+        // console.log("User: ", myThreadMessage2.content[0].text.value);
+        console.log("Assistant: ", allMessages.data[0].content[0].text.value);
+        result = allMessages.data[0].content[0].text.value;
+        break;
+      } else if (
+        keepRetrievingRun.status === "queued" ||
+        keepRetrievingRun.status === "in_progress"
+      ) {
+        // pass
+      } else {
+        console.log(`Run status: ${keepRetrievingRun.status}`);
+        console.log(`Error: ${keepRetrievingRun.last_error.message}`);
+        break;
+      }
+    }
+    return result;
+  };
+  return await retrieveRun();
+}
+
+export default fetchDifference;
--- utils/generate-completion.js
+++ utils/generate-completion.js
@@ -1,6 +1,7 @@
 import config from '../config/index.js';
 import { MOCK_TEXT_OK } from '../constants/mock.js';
 import { createChatCompletion, FINISH_REASON_STOP } from '../services/openai.js';
+import { fetchDifference } from './fetch-difference.cjs';
 
 class Completion {
   text;
@@ -31,12 +32,12 @@ const generateCompletion = async ({
   if (config.APP_ENV !== 'production') return new Completion({ text: MOCK_TEXT_OK });
   const { data } = await createChatCompletion({ messages: prompt.messages });
   const [choice] = data.choices;
-  console.log('data.choices:', choice.message);
-  if (choice.message.function_call != null) {
-    console.log('Function call:', choice.message.function_call);
+  let answer = choice.message.content.trim();
+  if (answer === 'Please upload your renewal policy quote and your current policy quote.') {
+    answer = await fetchDifference();
   }
   return new Completion({
-    text: choice.message.content.trim(),
+    text: answer,
     finishReason: choice.finish_reason,
   });
 };
