b17868991894aef62ce49693278cde7f30cb224f
Calvin Fang <cfang@guidewire.com>
2025-01-30 23:55:41 -0800

    debug 1

--- utils/generate-completion.js
+++ utils/generate-completion.js
@@ -1,7 +1,9 @@
 import config from '../config/index.js';
 import { MOCK_TEXT_OK } from '../constants/mock.js';
 import { createChatCompletion, FINISH_REASON_STOP } from '../services/openai.js';
-import fetchDifference from './fetch-difference.js';
+// import fetchDifference from './fetch-difference.js';
+import fs from 'fs';
+import path from 'path';
 
 class Completion {
   text;
@@ -30,15 +32,23 @@ const generateCompletion = async ({
   prompt,
 }) => {
   if (config.APP_ENV !== 'production') return new Completion({ text: MOCK_TEXT_OK });
-  const { data } = await createChatCompletion({ messages: prompt.messages });
+  console.log(`Prompt Message: ${prompt.messages}`);
+  let usersPath = path.join(process.cwd(), 'test.json');
+  const context = fs.readFileSync(usersPath, 'utf8');
+  console.log(`context: ${context}`);
+  const { data } = await createChatCompletion({ messages: [
+    {role: "system", content: `${context}`},
+    {role: "user", content: prompt.messages},
+  ],
+    });
   const [choice] = data.choices;
   let answer = choice.message.content.trim();
-  console.log(`Generated completion: ${answer}`);
-  if (answer === 'Please upload your renewal policy quote and your current policy quote.') {
-    console.log('Fetching difference');
-    answer = await fetchDifference();
-    console.log(`Generated difference: ${answer}`);
-  }
+  // console.log(`Generated completion: ${answer}`);
+  // if (answer === 'Please upload your renewal policy quote and your current policy quote.') {
+  //   console.log('Fetching difference');
+  //   answer = await fetchDifference();
+  //   console.log(`Generated difference: ${answer}`);
+  // }
   return new Completion({
     text: answer,
     finishReason: choice.finish_reason,
